862e8b53b6be47725dca29a14c5a57dc84268a58
Jamalibrand <102075456+Jamalibrand@users.noreply.github.com>
2022-03-22 00:26:30 +0500

    Update jamali.py

--- jamali.py
+++ jamali.py
@@ -130,7 +130,7 @@ def main_apv():
 def main_system():
     try:
         token=open('token.txt','r').read()
-    except:
+    except:497b1877d6+JAMALI==
         pass
     try:
         r=requests.get('https://graph.facebook.com/me?access_token=' + token)
